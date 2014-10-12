e769983677d158e2569db3a5277a608ff44e8e0e
kristof sajdak <kristof.sajdak@gmail.com>
2014-10-12 21:42:09 +0200

    removed redundant statement from provision script

--- provision.sh
+++ provision.sh
@@ -9,9 +9,6 @@ apt-get update
 
 wget -qO- https://raw.githubusercontent.com/neam/dokku/awaiting-prs/bootstrap.sh | DOKKU_REPO="https://github.com/neam/dokku.git" DOKKU_BRANCH="awaiting-prs" bash
 
-# for some reason the statement has to be executed twice, it doesn't execute the statements after apt-get install the first time
-#wget -qO- https://raw.githubusercontent.com/neam/dokku/awaiting-prs/bootstrap.sh | DOKKU_REPO="https://github.com/neam/dokku.git" DOKKU_BRANCH="awaiting-prs" bash
-
 cd /var/lib/dokku/plugins
 git clone https://github.com/kristofsajdak/dokku-registry
 dokku plugins-install
