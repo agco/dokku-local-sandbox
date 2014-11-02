08818f67d2afb7279361334a3ba1904ac6585613
kristof sajdak <kristof.sajdak@gmail.com>
2014-11-02 16:56:01 +0100

    added a bunch of improvements to provision.sh : - support for nginx hostnames > 46 chars - added placeholder for dokku debug mode - add extra swap to improve stability - install htop and mosh - install nsenter
    Added README.md file with installation instructions
    
    Added extra gitignore patterns

---
+++ README.md
@@ -0,0 +1,84 @@
+Prerequisites
+-------------
+Git
+Vagrant
+Virtualbox
+
+
+Getting Started
+---------------
+
+clone this repository and
+```
+vagrant up
+```
+
+
+Setup SSH config
+----------------
+
+Create a new key pair
+
+```
+ssh-keygen -t rsa -f ~/.ssh/id_dokku -N ""
+```
+
+register the key with sshcommand
+```
+cat ~/.ssh/id_dokku.pub | vagrant ssh -c "sudo sshcommand acl-add dokku progrium"
+```
+
+subsequently setup a ssh config file to create an ssh alias
+```
+vi ~/.ssh/config
+```
+
+add the following :
+
+```
+Host dokku_local
+HostName 10.0.0.2
+IdentityFile ~/.ssh/id_dokku
+PreferredAuthentications publickey
+RequestTTY yes
+User dokku
+```
+
+At this point you can exec dokku commands over ssh, e.g. :
+
+```
+ssh dokku_local help
+```
+
+This should print the available Dokku commands
+
+Deploy
+------
+
+First create the application
+```
+ssh dokku_local create node-js-getting-started
+```
+
+set some environment variables
+```
+ssh dokku_local config:set node-js-getting-started FOO=BAR
+...
+```
+
+Clone a git repo and cd into the dir :
+```
+git clone https://github.com/heroku/node-js-getting-started.git
+cd node-js-getting-started
+```
+
+Add a git remote to Dokku using the ssh alias :
+```
+git remote add progrium dokku_local:node-js-getting-started
+```
+
+And deploy
+```
+git push progrium master
+
+```
--- provision.sh
+++ provision.sh
@@ -9,6 +9,44 @@ apt-get update
 
 wget -qO- https://raw.githubusercontent.com/neam/dokku/awaiting-prs/bootstrap.sh | DOKKU_REPO="https://github.com/neam/dokku.git" DOKKU_BRANCH="awaiting-prs" bash
 
+
+# update nginx.conf to support longer than 46 character hostnames (default value 64 which equals 46 chars)
+
+sed -i 's/server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/' /etc/nginx/nginx.conf
+cat /etc/nginx/nginx.conf | grep server
+/etc/init.d/nginx restart
+
+# add a dokkurc file to easily turn on and off debug mode (https://github.com/progrium/dokku/wiki/Troubleshooting)
+
+echo '#export DOKKU_TRACE=1' > /home/dokku/dokkurc
+
+# add extra swap to make dokku more stable in oom situations (https://github.com/dotcloud/docker/issues/1555)
+
+if [ ! -f "/var/swap.1" ]; then
+    sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=4096
+    sudo /sbin/mkswap /var/swap.1
+    sudo /sbin/swapon /var/swap.1
+    echo '/var/swap.1 swap swap defaults 0 0' > /etc/fstab
+fi
+
+# install htop and mosh
+
+apt-get install -y -q htop mosh
+
+# nsenter
+
+if [ ! -f /usr/local/bin/nsenter ]; then
+    cd /tmp
+    curl https://www.kernel.org/pub/linux/utils/util-linux/v2.24/util-linux-2.24.tar.gz \
+         | tar -zxf-
+    cd util-linux-2.24
+    ./configure --without-ncurses
+    make nsenter
+    cp nsenter /usr/local/bin/
+    wget -qO- https://raw.githubusercontent.com/jpetazzo/nsenter/master/docker-enter > /usr/local/bin/docker-enter
+    chmod +x /usr/local/bin/docker-enter
+fi
+
 cd /var/lib/dokku/plugins
 git clone https://github.com/kristofsajdak/dokku-registry
 dokku plugins-install
